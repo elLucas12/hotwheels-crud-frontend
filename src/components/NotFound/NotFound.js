e977f4d423195ab17e95a72a752e2886390d0069
elLucas12 <l.zunho@edu.pucrs.br>
2024-09-09 17:40:03 -0300

    Estilização componente NotFound

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,12 +1,20 @@
+import { Box, Typography } from "@mui/material";
 import React from "react";
 
 export default function NotFound() {
   return(
-    <div>
-      <div className="default-header">
-        <h1>Not Found</h1>
-        <p>Página não encontrada!</p>
-      </div>
-    </div>
+    <Box sx={{
+      display: "flex",
+      flexDirection: "column"
+    }}>
+      <Box component="div" sx={{margin: "auto", textAlign: "center"}}>
+        <Typography gutterBottom variant="h3" component="div">
+          404 Not Found
+        </Typography>
+        <Typography variant="body2" sx={{ color: 'red' }}>
+          A página/requisição em questão não foi encontrada!
+        </Typography>
+      </Box>
+    </Box>
   );
 }
